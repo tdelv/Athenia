12da33daaef54b9b7203aa0dff5b59f748c19b9d
Thomas Del Vecchio <thomasdelvecchio@Thomass-MacBook-Pro.local>
2019-03-30 22:41:18 -0400

    Set up project; built basic package structure; began work on Drive API.

---
+++ src/main/java/edu/brown/cs/athenia/main/Main.java
@@ -0,0 +1,18 @@
+package edu.brown.cs.athenia.main;
+
+import edu.brown.cs.athenia.driveapi.DriveQuickstart;
+
+public class Main {
+
+    /**
+     * Main function for Athenia project. Sets up Drive API and server.
+     * @param args the args passed in from command line.
+     */
+    public static void main(String[] args){
+        try {
+            DriveQuickstart.setup();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
