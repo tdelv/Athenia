77eee60b66789226b27f36f8ab17004994666af5
Thomas Del Vecchio <thomasdelvecchio@thomass-mbp.devices.brown.edu>
2019-04-01 16:24:39 -0400

    Comment out unecessary code.

--- src/main/java/edu/brown/cs/athenia/main/Main.java
+++ src/main/java/edu/brown/cs/athenia/main/Main.java
@@ -3,6 +3,17 @@ package edu.brown.cs.athenia.main;
 //import joptsimple.OptionParser;
 
 import edu.brown.cs.athenia.driveapi.GoogleDriveApi;
+import freemarker.template.Configuration;
+import spark.ExceptionHandler;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 
 public class Main {
@@ -33,6 +44,62 @@ public class Main {
             GoogleDriveApi.setup();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+
+    // Adding GUI
+
+    /**
+     * Creates the engine to run the Spark server.
+     * @return The FreeMarkerEngine to run the server.
+     */
+    private static FreeMarkerEngine createEngine() {
+        Configuration config = new Configuration(); // Configuration.VERSION_2_3_25);
+        File templates = new File("src/main/resources/spark/template/freemarker");
+        try {
+            config.setDirectoryForTemplateLoading(templates);
+        } catch (IOException ioe) {
+            System.out.printf("ERROR: Unable use %s for template loading.%n",
+                    templates);
+            System.exit(1);
+        }
+        return new FreeMarkerEngine(config);
+    }
+
+
+    /**
+     * Open the server on the local port.
+     * @param port
+     *          The port for server to run on.
+     */
+    @SuppressWarnings("unchecked")
+    private void runSparkServer(int port) {
+        Spark.port(port);
+        Spark.externalStaticFileLocation("src/main/resources/static");
+        Spark.exception(Exception.class, new ExceptionPrinter());
+
+        FreeMarkerEngine freeMarker = createEngine();
+
+        // Setup Spark Routes
+        //Spark.get("/stars", new StarsHandlers.FrontHandler(), freeMarker);
+    }
+
+    /**
+     * Display an error page when an exception occurs in the server.
+     * @author Thomas Del Vecchio
+     */
+    private static class ExceptionPrinter implements ExceptionHandler {
+        @Override
+        public void handle(Exception e, Request req, Response res) {
+            res.status(500);
+            StringWriter stacktrace = new StringWriter();
+            try (PrintWriter pw = new PrintWriter(stacktrace)) {
+                pw.println("<pre>");
+                e.printStackTrace(pw);
+                pw.println("</pre>");
+            }
+            res.body(stacktrace.toString());
         }
     }
 }
