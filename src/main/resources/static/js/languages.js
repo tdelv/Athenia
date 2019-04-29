87f82adda633f5d919f311a99513d6564d81a0bb
Mia Santomauro <mia_santomauro@brown.edu>
2019-04-29 10:09:29 -0400

    adds ftl files

--- src/main/resources/static/js/languages.js
+++ src/main/resources/static/js/languages.js
@@ -1,4 +1,20 @@
 
 $( document ).ready(function() {
 
+    $("#addLanguageButton").click(addNewLanguage);
+
 });
+
+function addNewLanguage() {
+
+    console.log("adding new language");
+
+    const newLanguage = $("#languageInput").val();
+
+    console.log("new lang: " + newLanguage);
+
+/*
+
+ */
+
+}
