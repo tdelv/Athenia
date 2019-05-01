bc2ffec8a3259544156b2a27119432f556d0d90b
Thomas Del Vecchio <thomasdelvecchio@thomass-mbp.devices.brown.edu>
2019-05-01 00:14:07 -0400

    Began adding database parsing for getting user.

--- src/main/java/edu/brown/cs/athenia/databaseparser/DatabaseParser.java
+++ src/main/java/edu/brown/cs/athenia/databaseparser/DatabaseParser.java
@@ -1,22 +1,23 @@
 package edu.brown.cs.athenia.databaseparser;
 
-import edu.brown.cs.athenia.driveapi.UnauthenticatedUserException;
-import edu.brown.cs.athenia.main.Athenia;
-import edu.brown.cs.athenia.data.modules.FreeNote;
+import edu.brown.cs.athenia.data.Language;
+import edu.brown.cs.athenia.data.modules.Module;
 import edu.brown.cs.athenia.data.modules.module.Conjugation;
 import edu.brown.cs.athenia.data.modules.module.Note;
-import edu.brown.cs.athenia.data.modules.module.Tag;
 import edu.brown.cs.athenia.data.modules.module.Vocab;
+import edu.brown.cs.athenia.main.Athenia;
 import edu.brown.cs.athenia.driveapi.DriveApiException;
 import edu.brown.cs.athenia.driveapi.GoogleDriveApi;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.Instant;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -29,35 +30,38 @@ public class DatabaseParser {
     private static final Map<String, Athenia> USER_MAP = new HashMap<>();
 
 
-    private static Connection loadConnection(String userId) throws DatabaseParserException, SQLException {
-        File file;
-        try {
-            file = GoogleDriveApi.getDataBase(userId);
-        } catch (DriveApiException e) {
-            throw new DatabaseParserException(e);
-        }
-
-        if (!file.exists()) {
-            try {
-                setup(userId, file);
-            } catch (IOException e) {
-                throw new DatabaseParserException(e);
-            }
-        }
-
-        try {
-            Class.forName("org.sqlite.JDBC");
-            String url = "jdbc:sqlite:" + file.getPath();
-            return DriverManager.getConnection(url);
-        } catch (ClassNotFoundException e) {
-            throw new DatabaseParserException(e);
-        }
+    private static Connection getConnection(String filePath) throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        String url = "jdbc:sqlite:" + filePath;
+        return DriverManager.getConnection(url);
     }
 
-    private static void setup(String userAuth, File file) throws IOException, DatabaseParserException, SQLException {
+    private static Connection loadConnection(String userId)
+            throws SQLException, ClassNotFoundException, IOException, DriveApiException {
+        File file = GoogleDriveApi.getDataBase(userId);
+
+        if (!file.exists()) {
+            setup(userId, file);
+        }
+
+        return getConnection(file.getPath());
+    }
+
+    private static void setup(String userId, File file) throws IOException, SQLException, ClassNotFoundException {
+        File queryFile = new File("/SQLCommands/setup_database");
+        String data;
+        try (BufferedReader reader = new BufferedReader(new FileReader(queryFile))) {
+            data = reader.lines().reduce("", (acc, ele) -> acc + "\n" + ele);
+        }
+
+        String[] queries = data.split("\n");
+
         file.createNewFile();
-        try (Connection conn = DriverManager.getConnection(file.getPath());
-                Statement stmt = conn.createStatement()) {
+        try (Connection conn = getConnection(file.getPath());
+                Statement statement = conn.createStatement()) {
+            for (String query : queries) {
+                statement.execute(query);
+            }
         }
     }
 
@@ -68,6 +72,85 @@ public class DatabaseParser {
 
         Athenia user = new Athenia(userId);
         USER_MAP.put(userId, user);
+
+        try (Connection conn = loadConnection(userId);
+                Statement statement = conn.createStatement()) {
+            // Get meta data for user
+            try (ResultSet rs = statement.executeQuery(
+                    "SELECT username, joined, last_update FROM user_data")) {
+
+            }
+
+            // Get languages
+            try (ResultSet rs = statement.executeQuery(
+                    "SELECT language FROM languages")) {
+                user.addLanguage(rs.getString(1));
+            }
+
+            // Fill in languages
+            for (String langName : user.getLanguages()) {
+                user.setCurrLang(langName);
+                Language language = user.getCurrLanguage();
+
+                int id;
+                String type;
+                int created;
+                int lastModified;
+                try (ResultSet rs = statement.executeQuery(
+                        "SELECT m.id, m.type, m.created, m.last_modified FROM " +
+                                "modules AS m, languages AS l WHERE " +
+                                "m.language_id = l.id AND " +
+                                "l.language = " + langName)) {
+                    while (rs.next()) {
+                        id = rs.getInt(1);
+                        type = rs.getString(2);
+                        created = rs.getInt(3);
+                        lastModified = rs.getInt(4);
+
+                        Module module;
+                        switch (type) {
+                            case "note":
+                                try (ResultSet rs2 = statement.executeQuery(
+                                        "SELECT body FROM note_modules WHERE " +
+                                                "id = " + id)) {
+                                    module = new Note(rs.getString(1));
+                                }
+                                break;
+                            case "vocab":
+                                try (ResultSet rs2 = statement.executeQuery(
+                                        "SELECT term, definition FROM vocab_modules WHERE " +
+                                                "id = " + id)) {
+                                    module = new Vocab();
+                                    module.update(new String[]{rs.getString(1), rs.getString(2)});
+                                }
+                                break;
+//                            case "conjugation":
+//                                try (ResultSet rs2 = statement.executeQuery(
+//                                        "SELECT header FROM vocab_modules WHERE " +
+//                                                "id = " + id)) {
+//                                    module = new Conjugation();
+//                                    module.update(new String[]{rs.getString(1), rs.getString(2)});
+//                                }
+//                                break;
+                            default:
+                                module = null;
+                                break;
+
+                        }
+                        module.setId(Integer.toString(id));
+                        module.setDateModified(Date.from(Instant.ofEpochMilli(lastModified)));
+                    }
+                }
+
+
+            }
+
+        } catch (SQLException | ClassNotFoundException | IOException e) {
+            throw new DatabaseParserException(e);
+        } catch (DriveApiException e) {
+            throw new DatabaseParserException(e);
+        }
+        List<String> words = Arrays.asList("hello", "goodbye");
         return user;
     }
 }
