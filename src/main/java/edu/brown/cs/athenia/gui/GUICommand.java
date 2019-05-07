6ccc31e1d78ec65699427b7a70007919ed6894e4
Jason Hebert <hebert.jason97@gmail.com>
2019-05-07 09:52:46 -0400

    committtting

--- src/main/java/edu/brown/cs/athenia/gui/GUICommand.java
+++ src/main/java/edu/brown/cs/athenia/gui/GUICommand.java
@@ -2577,26 +2577,26 @@ public class GUICommand {
 
             // TODO IDK HOW TO FIX THIS
             // create ReviewMode object
-//            ReviewMode reviewer =
-//                    new ReviewMode(user, tagList, startDateObject, endDateObject);
-//            // get list of reviewables
-//
-//            List<Reviewable> reviewablesList = reviewer.review();
-//
-//            System.out.println(reviewablesList.isEmpty());
-//
-//            for (Reviewable r : reviewablesList) {
-//              System.out.println(r.getRating());
-//            }
+            ReviewMode reviewer =
+                    new ReviewMode(user, tagList, startDateObject, endDateObject);
+            // get list of reviewables
 
-            // TODO THESE ARE TESTS TEST TEST TEST
-            Vocab vocabNew = new Vocab("uwu", "owo");
-            Note noteNew = new Note("uwuwuwuwuwuw wwowowow");
+            List<Reviewable> reviewablesList = reviewer.review();
 
-            List<Reviewable> reviewablesList = new ArrayList<>();
-            reviewablesList.add(vocabNew);
-            reviewablesList.add(noteNew);
-            // TODO THESE ARE TESTS TESTS TESTS
+            System.out.println(reviewablesList.isEmpty());
+
+            for (Reviewable r : reviewablesList) {
+              System.out.println(r.getRating());
+            }
+
+//            // TODO THESE ARE TESTS TEST TEST TEST
+//            Vocab vocabNew = new Vocab("uwu", "owo");
+//            Note noteNew = new Note("uwuwuwuwuwuw wwowowow");
+//
+//            List<Reviewable> reviewablesList = new ArrayList<>();
+//            reviewablesList.add(vocabNew);
+//            reviewablesList.add(noteNew);
+//            // TODO THESE ARE TESTS TESTS TESTS
 
 
             // prepare list of modules to review for frontend
